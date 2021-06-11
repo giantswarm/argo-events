069ee3e54bbc749964cbe303c86b11b233d8a4db
kubasobon <wtty.fool@gmail.com>
2021-06-11 12:28:05 +0200

    split resources into CRDs and templates

--- hack/update-version.sh
+++ hack/update-version.sh
@@ -1,11 +1,21 @@
 #! /bin/bash
 
 USED_VER="${VERSION:-stable}"
+CRD_FILE=helm/argo-events/crds/crds.yaml
+RESOURCE_FILE=helm/argo-events/templates/resources.yaml
+TMP_FILE=/tmp/argo-events-$USED_VER.yaml
+
 echo "Updating templates using argo-events version $USED_VER..."
 
 sed -i "s/stable/$USED_VER/" manifests/bases/kustomization.yaml
-echo "# This is an auto-generated file. DO NOT EDIT" > helm/argo-events/templates/resources.yaml
-echo "# VERSION=$USED_VER" >> helm/argo-events/templates/resources.yaml
-kubectl kustomize manifests/namespaced >> helm/argo-events/templates/resources.yaml
-git checkout -- manifests/bases/kustomization.yaml
+kubectl kustomize manifests/namespaced > $TMP_FILE
 
+echo "# This is an auto-generated file. DO NOT EDIT" > $CRD_FILE
+echo "# VERSION=$USED_VER" >> $CRD_FILE
+cat $TMP_FILE | yq -y 'select(.kind == "CustomResourceDefinition")' >> $CRD_FILE
+
+echo "# This is an auto-generated file. DO NOT EDIT" > $RESOURCE_FILE
+echo "# VERSION=$USED_VER" >> $RESOURCE_FILE
+cat $TMP_FILE | yq -y 'select(.kind != "CustomResourceDefinition")' >> $RESOURCE_FILE
+
+git checkout -- manifests/bases/kustomization.yaml
