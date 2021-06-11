5fd7faf067dd230f69147b3792cc9f8678b8effc
kubasobon <wtty.fool@gmail.com>
2021-06-11 10:57:40 +0200

    append version to generated file

--- hack/update-version.sh
+++ hack/update-version.sh
@@ -4,6 +4,8 @@ USED_VER="${VERSION:-stable}"
 echo "Updating templates using argo-events version $USED_VER..."
 
 sed -i "s/stable/$USED_VER/" manifests/bases/kustomization.yaml
-kubectl kustomize manifests/namespaced > helm/argo-events/templates/resources.yaml
+echo "# This is an auto-generated file. DO NOT EDIT" > helm/argo-events/templates/resources.yaml
+echo "# VERSION=$USED_VER" >> helm/argo-events/templates/resources.yaml
+kubectl kustomize manifests/namespaced >> helm/argo-events/templates/resources.yaml
 git checkout -- manifests/bases/kustomization.yaml
 
